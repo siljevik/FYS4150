5e2ad781aa85b0229c9b087b073a29c3a2c2ee89
sanderhi <s.h.iden@fys.uio.no>
2022-09-18 13:20:23 +0200

    Project 2 directory added

---
+++ Project_2/3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <armadillo>
+
+// problem 3
+using namespace arma;
+
+double max_offdiag_symmetric(const arma::mat A, int&k, int &l)
+{
+// create a matrix and make an  identifier for the largest off-diagonal
+return 0;
+}
