c03c301009fe736c8d3c58674155e4d1b08c5bb6
siljevik <siljevik_94@hotmail.com>
2022-09-22 18:45:57 +0200

    almost done commenting prob4

--- Project_2/4/prob4.cpp
+++ Project_2/4/prob4.cpp
@@ -8,75 +8,69 @@
 
 #include <iostream>
 #include <armadillo>
-using namespace arma;
+using namespace arma;   // so we don't have to use 'arma::' before every armadollon operation
 
-// Declarations
-double offdiag(mat& A, int& p, int& q, int N);
+// Declarations before our main
+double offdiag(mat& A, int& p, int& q, int N); // The '&' denotes reference to an object, so we can change it
 void jacobi_rotate(mat& A, mat& R, int k, int l, int N);
 mat eigen_solver_loop(double tolerance, mat& A, mat& R, int N, int its_max, vec& eigenvals_A) ;
 
 //////////////////  MAIN
-int main() {
-    /////////////// FROM PROBLEM 2
-
-    // big N to adjust the size of the NxN matrix A
-        int N = 6;  // From Praoblem 4 b)
-    // minimum and maximum points
-        double x_min = 0.0;
-        double x_max = 1.0;
-        int  n = N + 1;
-        double dx = x_max - x_min;
-        double h = dx*1./n;
-    // create an empty matrix A, using Armadillo
-        mat A(N, N, fill::zeros);
-        mat R(N, N, fill::eye);  // Matrix R to be filled with the eigenvectors
+int main() 
+{
+    int N = 6;          // Size of matrix A is NxN
+    double x_min = 0.0; // min point
+    double x_max = 1.0; // max point
+    int  n = N + 1;
+    double dx = x_max - x_min;
+    double h = dx*1./n;
+    mat A(N, N, fill::zeros); // Creating an empty matrix A, of size NxN, with Armadillo
+    mat R(N, N, fill::eye);  // Matrix R to be filled with the eigenvectors
     // Creating two loops to fill the tridiagonal matrix with values:
-    // values, a ,diag.
-        for (int i=0; i<N; i++){
-        A(i, i) =  2./(h*h);
-        }
+    for (int i=0; i<N; i++)
+        {A(i, i) =  2./(h*h);}
     // values,c ,diag.
-        for (int i=0; i<(N-1) ; i++){
-        A(i+1, i) = -1./(h*h);
-        A(i, i+1) = -1./(h*h);
-        }
-        A(3,4) = 51;
+    for (int i=0; i<(N-1) ; i++)
+        {A(i+1, i) = -1./(h*h);
+         A(i, i+1) = -1./(h*h);}
+    // Just to have a look at the matrix we just made
     std::cout << "\n Original Matrix A: \n";
     std::cout << A << "\n";
 
-    /////////////
-    // we want to solve Av = lambda v using arma::eig_sym
+    // Solving A*v = lambda*v using arma::eig_sym
 	vec eigval; // vector containing eigenvalues
-	mat eigvec; // eigenvector
+	mat eigvec; // matrix containint eigenvectors
 	eig_sym(eigval, eigvec, A);
-    // eigenvalues in order
-    std::cout << "Eigen-values: \n" << eigval << "\n";
-    // eigevectors in a matrix
-    std::cout << "Eigen-vectors: \n"<< eigvec << "\n";
-    ////////////
+    // eigenvalues of our matrix, in increasing order
+    std::cout << "Original Eigen-values: \n" << eigval << "\n";
+    // Eigevectors of our matrix, where the first column belongs to first entry in the eigval
+    // vector, and the second column belongs to the second entry etc.
+    std::cout << "Original Eigen-vectors: \n"<< eigvec << "\n";
+    
     int p = 0;
     int q = 0;
     double max;
-    max = offdiag(A, p, q,N);
+    max = offdiag(A, p, q,N); // Finds the largest number/entry in the matrix that is not in the diagonal
     
-    double tolerance = pow(10, -8); // pow(base, exponent)
-    int its_max = 10000;
-    vec eigenvals_A(N);//,sorted_eigenvals(N); // defining vector with N elements
-    R = eigen_solver_loop(tolerance, A, R, N, its_max, eigenvals_A);
+    double tolerance = pow(10, -8); // pow(base, exponent) 'ten in the power of minus eight'
+    int its_max = 10000;            // Let's not let our program go too crazy:D
+    vec eigenvals_A(N);             // defining vector with N elements
+    R = eigen_solver_loop(tolerance, A, R, N, its_max, eigenvals_A); // Creating a matrix where each column is an eigenvector
 
     
 
     // sorting_bysize(R,eigenvals_A, sorted_R, sorted_eigenvals_A);
+
+    // Printing our updated matrixes neatly (imo)
     std::cout << "New matrix A: \n" << A << "\n";
     std::cout << "Matrix R: \n" << R << "\n";
     
 
-////////////////////////////////
-    // using armadillo to note the indices 
-    uvec indices = sort_index(eigenvals_A);
+    ////////////////////////////////
+    uvec indices = sort_index(eigenvals_A); // using armadillo to note the indices 
     std::cout << "Sorted (now just index) eigenvals A: \n"<< indices << "\n";
-    vec sorted_eigenvals(N);
-    mat sorted_R(N,N);
+    vec sorted_eigenvals(N);    // Creating a vector, sorted_eigenvals, of length N
+    mat sorted_R(N,N);          // Creating a matrix, sorted_R, of size NxN
     for (int i = 0; i<N; i++){
         sorted_eigenvals(i) = eigenvals_A(indices(i));
         for(int j=0; j<N;j++){
@@ -85,18 +79,14 @@ int main() {
     }
     std::cout << "Sorted matrix R \n" << sorted_R << "\n";
     ////////////////////////////////
-
-    //A.print(std::cout);
-    //std::cout << "jacobi_rotate matrix R (????idk) \n";
-    //R.print(std::cout);
     return 0;
 }
 
 
 ////////////////// OFF DIAGONAL
-        // Determine the the max off-diagonal element of a symmetric matrix A
-        // - Saves the matrix element indicies to k and l 
-        // - Returns absolute value of A(k,l) as the function return value
+    // Determine the the max off-diagonal element of a symmetric matrix A
+    // - Saves the matrix element indicies to k and l 
+    // - Returns absolute value of A(k,l) as the function return value
     double offdiag(mat& A, int& p, int& q, int N) ///////////// OPPGAVE 3!!!   // & er reference/pointer
     {
             double max;
@@ -104,17 +94,17 @@ int main() {
             {
                 for ( int j = i+1; j < N; ++j)
                 {
-                    double aij = fabs(A(i,j));
+                    double aij = fabs(A(i,j)); // Absolute value of A(i,j)
                     if ( aij > max)
                     {
-                        max = aij; p = i; q = j;
+                        max = aij; p = i; q = j; // Updating our values/numbers
                     }
                 }
             
             }
-            return max; 
+            return max; // Returns the max off-diagonal element of A
         }
-
+///////////////////////////////////////////////////// KOMMENTER FERDIG UNDER HER SILJE
 ////////////////// JACOBI ROTATION
     // Performs a single Jacobi rotation, to "rotate away"
     // the off-diagonal element at A(k,l).
