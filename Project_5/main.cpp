6803b59e90dbdc2ed0158a95a15ad07bb3502088
siljevik <siljevik_94@hotmail.com>
2022-12-09 17:01:01 +0100

    updated main

--- Project_5/main.cpp
+++ Project_5/main.cpp
@@ -15,13 +15,29 @@ int main(){
     /*~~~~ Constants, Variables, etc. ~~~~*/
     /*====================================*/
 	int M = 5; // M-2 = 3 Here
-	std::complex<double> r_val = 1.0;
+	int h = 1;
+	int dt = 1;
+	arma::cx_double r_val = 1.0;
+
+	/*===========================================*/
+    /*~~~~ Making matrices filled with zeros ~~~~*/
+    /*===========================================*/
+	arma::mat V(M,M,fill::ones);
+	arma::mat A(pow(M-2,2), pow(M-2,2), fill::zeros);
+	arma::mat B(pow(M-2,2), pow(M-2,2), fill::zeros);
+
+
 	// Define an empty vector that goes through a function that returns
 	// a full vector dependent on indices (i,j) that we can run through with a loop
 	cout << "Testing with matrix filled with ones for problem 2:" << endl;
 	cout << head.index_translator(M) << endl;
 
 	head.matrix_filler(M,r_val);
- return 0;
+
+	diagonal_fill_AB(int M, int h, int dt, V, & A,& B)
+
+
+
+ 	return 0;
 
 }
